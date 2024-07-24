6725da4c973906c183a6946c6bf260a73c938140
Brenton <brentonxie@gmail.com>
2024-07-24 19:32:54 +1000

    almost complete

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import Navbar from "./Components/Narbar/Navbar";
 import Intro from "./Components/Intro/Intro";
 import './App.css';
 import Services from "./Components/Services/Services";
+import Experience from "./Components/Experience/Experience";
+import Works from "./Components/Works/Works";
+import Portfolio from "./Components/Portfolio/Portfolio";
+import Contact from "./Components/Contact/Contact";
+import { themeContext } from "./Context";
+import { useContext } from "react";
+
 
 function App() {
+  const theme = useContext(themeContext)
+  const darkMode = theme.state.darkMode
   return (
-    <div className="App">
+    <div className="App"
+      style={{
+        background: darkMode ? 'black' : '',
+        color: darkMode ? 'white' : ''
+      }}
+    >
       <Navbar />
       <Intro />
       <Services />
+      <Experience />
+      <Works />
+      <Portfolio />
+      <Contact />
     </div>
   );
 }
--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -5,12 +5,16 @@ import Glasses from "../../img/glasses.png";
 import Humble from "../../img/humble.png";
 import Card from '../Card/Card';
 import Resume from './Resume.pdf';
+import { themeContext } from "../../Context";
+import { useContext } from "react";
 
 const Services = () => {
+  const theme = useContext(themeContext)
+  const darkMode = theme.state.darkMode
   return (
-    <div className="services">
+    <div className="services" id='Services'>
       <div className="awesome">
-        <span>My Awesome</span>
+        <span style={{ color: darkMode ? 'white' : '' }}>My Awesome</span>
         <span>services</span>
         <span>
           Lorem ispum is simpley dumy text of printing of printing Lorem
@@ -20,7 +24,7 @@ const Services = () => {
         <a href={Resume} download>
           <button className="button s-button">Download CV</button>
         </a>
-        <div className="blur s-blur" style={{ background: '#ABF1FF94' }}></div>
+        <div className="blur s-blur1" style={{ background: '#ABF1FF94' }}></div>
       </div>
       <div className="cards">
         <div style={{ left: '14rem' }}>
@@ -43,6 +47,9 @@ const Services = () => {
             heading={'UI/UX'}
             detail={'Lorem ispum dumMy are usually use in section where we need some random text'}
           />
+        </div>
+        <div>
+          <div className="blur s-blur2" style={{ background: 'var{--purple}' }}></div>
         </div>
       </div>
     </div>
